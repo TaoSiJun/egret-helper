9029e52f778b212a1cdae260b5eb35d87fbb7d04
taosijun <taosj@lerjin>
2020-11-11 10:43:56 +0800

    更新国际化

--- src/manager/I18n.ts
+++ src/manager/I18n.ts
@@ -1,36 +1,31 @@
-namespace h {
-    export class I18n {
-        private _data: { [key: string]: string };
+namespace h.i18n {
+    let i18nMetadata: { [key: string]: string };
 
-        public setData(value: any) {
-            this._data = value;
-        }
-
-        /**
-         * @example
-         * {key:'hello {0} {1}'}
-         * i18n.localize('key','world1','world2') //'hello world1 world2'
-         */
-        public localize(key: string, ...args: any[]) {
-            try {
-                let s = this._data[key];
-                if (!s) {
-                    throw key;
-                }
-                for (let i = 0; i < args.length; ++i) {
-                    s = s.replace("{" + i + "}", args[i]);
-                }
-                return s;
-            } catch (error) {
-                console.warn(`i18n ${key} not found`);
-            }
-        }
+    /**
+     * 设置语言包数据
+     * @param data
+     */
+    export function setI18nMetadata(data: any) {
+        i18nMetadata = data;
     }
 
     /**
-     * 国际化
-     * @method setData 初始化本地语言数据
-     * @method localize 通过key获取本地语言
+     * 本地化
+     * @param key 语言包key
+     * @param args 占位符参数
      */
-    export const i18n = new I18n();
+    export function localize(key: string, ...args: any[]) {
+        try {
+            let s = i18nMetadata[key];
+            if (!s) {
+                throw key;
+            }
+            for (let i = 0; i < args.length; ++i) {
+                s = s.replace("{" + i + "}", args[i]);
+            }
+            return s;
+        } catch (error) {
+            console.warn(`i18n ${key} not found`);
+        }
+    }
 }
